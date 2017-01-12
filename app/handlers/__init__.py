4c8ab0ffe89e57f48e6a6583d990e09dd378155f
FrolovAleksandr <alfrobox@gmail.com>
2017-01-12 08:45:39 +0000

    fix for Selectel

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,5 +1,4 @@
 import datetime
-from enum import Enum
 from flask_restful import Resource
 from flask_responses import json_response
 
@@ -9,12 +8,7 @@ class BaseHandler(Resource):
         self.date_time_now = datetime.datetime.utcnow()
 
     def success_response(self, data):
-        return json_response(data, status_code=RESPONSE_CODE.success)
+        return json_response(data, status_code=200)
 
     def bad_response(self, message):
-        return json_response({"message": message}, status_code=RESPONSE_CODE.bad_request)
-
-
-class RESPONSE_CODE(Enum):
-    success = 200
-    bad_request = 400
+        return json_response({"message": message}, status_code=400)
